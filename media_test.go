bda951077a9da29ad7addecb8de07ebee632e27f
Johnny Debt <38006769+johnny-debt@users.noreply.github.com>
2018-06-28 23:41:50 +0300

    New test added for medias retrieving error handling.

--- media_test.go
+++ media_test.go
@@ -31,6 +31,27 @@ func TestGetHashtagMediaSuccessful(t *testing.T) {
 	validateMediaObjects(t, medias)
 }
 
+// Ensures that when JSON retrieving occurred this method returns error as well
+func TestGetHashtagMediaJSONRetrievingError(t *testing.T) {
+	defer gock.Off()
+	hashtag := "something"
+	maxId := ""
+	apiEndpoint := "https://www.instagram.com"
+	apiUri := fmt.Sprintf("explore/tags/%s", hashtag)
+	params := map[string]string{"__a": "1", "max_id": maxId}
+
+	gock.New(apiEndpoint).
+		Get(apiUri).
+		MatchParams(params).
+		Reply(201).
+		JSON("")
+
+	medias, err := GetHashtagMedia(hashtag)
+
+	assert.Error(t, err)
+	assert.Nil(t, medias)
+}
+
 func validateMediaObjects(t *testing.T, medias []Media) {
 	for _, media := range medias {
 		validateMediaObject(t, media)
